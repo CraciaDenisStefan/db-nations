59e3db3df6b1d9452ffdcedf5d867cbd6d2ea6b5
CraciDenisStefan <crdenis13@gmail.com>
2023-11-24 15:33:36 +0100

    Milestone 3 ok

--- src/main/java/org/java/Main.java
+++ src/main/java/org/java/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -13,18 +14,27 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Filtra tra i paesi inserendo delle lettere: ");
+		
+		String Filtro = in.nextLine();
+		
 		try (Connection con = DriverManager.getConnection(url, user, pws)) {  
-			  
+			
+			
 			  final String SQL = "SELECT countries.country_id , countries.name ,regions.name ,continents.name "
 					 +" FROM countries "
 					 +" JOIN regions "
 					 +" 	ON countries.region_id = regions.region_id "
 					 +" JOIN continents"
 					 +" 	ON regions.continent_id = continents.continent_id "
-					 + " ORDER BY countries.country_id  "
+					 +"  WHERE countries.name LIKE ?"
+					 + " ORDER BY countries.name  "
 					 +" ; ";
 			  
 			  try (PreparedStatement ps = con.prepareStatement(SQL)){
+				  
+				  ps.setString(1,  "%" + Filtro + "%");
 				  
 			    try (ResultSet rs = ps.executeQuery()){
 			    	
@@ -36,7 +46,7 @@ public class Main {
 			    		final String NOME_CONTINENTE = rs.getString(4);
 			    		
 			    		
-			    		System.out.println(ID +" "+ NOME +" " +   NOME_REGIONE +" " + NOME_CONTINENTE );
+			    		System.out.println( NOME + " " + ID +" "+   NOME_REGIONE +" " + NOME_CONTINENTE );
 			    	}
 			    }
 			  }
